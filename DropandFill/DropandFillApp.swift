148ad673477fa711632c1ef6cb83131926db81b6
Abdulganiy-dev <labdughaneeydev@gmail.com>
2026-08-05 14:14:26 +0100

    Initial Commit

---
+++ DropandFill/DropandFillApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropandFillApp.swift
+//  DropandFill
+//
+//  Created by ABDULGANIY LAWAL on 05/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct DropandFillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
